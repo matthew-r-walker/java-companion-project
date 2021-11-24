e5ae7ff1841439880ae36ac397fe147a9102b237
matthew.b.walker <matthew.b.walker@AFSWWR023BMQLQ.us.accenturefederal.com>
2021-11-24 15:00:06 -0600

    Removed unnecessary reference on @Service annotation

--- mgl-webapp/src/main/java/com/organization/mvcproject/MGL_Task1/service/Game_Service_Impl.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/MGL_Task1/service/Game_Service_Impl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import com.organization.mvcproject.MGL_Task1.model.Game;
 
 //TODO 1.0  follow java class naming, improve class name
-//TODO 1.0  "javaGameService" service reference name is not necessary, remove it.
+//TODONE 1.0  "javaGameService" service reference name is not necessary, remove it.
 @Service
 public class Game_Service_Impl implements Game_Service {
 
