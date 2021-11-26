b418daea8831b7ff8022cb0c7fb780cae4a43bec
matthew.b.walker <matthew.b.walker@AFSWWR023BMQLQ.us.accenturefederal.com>
2021-11-26 09:12:39 -0600

    Renamed request mapping URLs for fetch all games and create game

--- mgl-webapp/src/main/java/com/organization/mvcproject/MGL_Task1/controller/MGLTask1Controller.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/MGL_Task1/controller/MGLTask1Controller.java
@@ -68,14 +68,14 @@ public class MGLTask1Controller {
 	 * TODO 2.0 (Separation of concerns) consider moving all controller endpoints that return a ResponseEntity into a @RestController.
 	 */
 	
-	//TODO 1.0 RequestMapping URL should follow RESTful.
-	@RequestMapping(value = "/game/getAll", method = RequestMethod.GET)
+	//TODONE 1.0 RequestMapping URL should follow RESTful.
+	@RequestMapping(value = "/game/all", method = RequestMethod.GET)
 	public ResponseEntity<List<Game>> fetchAllGames() {
 		return new ResponseEntity<List<Game>>(javaGameService.retrieveAllGames(), HttpStatus.OK);
 	}
 
-	//TODO 1.0 RequestMapping URL should follow RESTful convention
-	@RequestMapping(value = "/game/createGame", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	//TODONE 1.0 RequestMapping URL should follow RESTful convention
+	@RequestMapping(value = "/game/create", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Void> createGame(@RequestBody Game game) {
 		javaGameService.saveGame(game);
 		return new ResponseEntity<Void>(HttpStatus.CREATED);
