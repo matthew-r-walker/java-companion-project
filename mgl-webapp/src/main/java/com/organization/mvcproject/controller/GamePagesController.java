548daaf61c0c74bd9b42242aea96c8e0fec58b13
matthew.b.walker <matthew.b.walker@AFSWWR023BMQLQ.us.accenturefederal.com>
2021-12-08 14:25:43 -0600

    Removed unused packages

--- mgl-webapp/src/main/java/com/organization/mvcproject/controller/GamePagesController.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/controller/GamePagesController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+
 
 import com.organization.mvcproject.dao.IGameDao;
 import com.organization.mvcproject.model.Game;
