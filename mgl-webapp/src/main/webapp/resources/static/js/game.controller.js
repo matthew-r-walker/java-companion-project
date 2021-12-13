7e3b6b2611780ac5344eaefe19f86ee3076993ab
matthew.b.walker <matthew.b.walker@AFSWWR023BMQLQ.us.accenturefederal.com>
2021-12-13 09:51:59 -0600

    Added genre filter

--- mgl-webapp/src/main/webapp/resources/static/js/game.controller.js
+++ mgl-webapp/src/main/webapp/resources/static/js/game.controller.js
@@ -9,16 +9,47 @@ angular.module('MGL_Task1_app').controller('MGL_Task1_Controller',
 				genre : ''
 			};
 			self.games = [];
+			self.genres = [{name: 'All'}]; 
+			self.selectedGenre = 'All';
+			
+			
 
 			self.fetchAllGames = function(){
-				MGL_Task1_Service.fetchAllGames().then(function(data) {
-					self.games = data;
-				});
+				if(self.selectedGenre == 'All'){
+					MGL_Task1_Service.fetchAllGames().then(function(data) {
+						self.games = data;
+						if(self.games.length > 0){
+							self.genres = parseGenres();	
+						}else{
+							self.genres = [{name: 'All'}];//clear
+						}
+					})
+				} else {
+					MGL_Task1_Service.filterByGenre(self.selectedGenre).then( function(data) {
+						self.games = data;
+					});
+				}
+			}
+			
+			function parseGenres(){
+				var strArray = [];
+				var genreArray = [{name: 'All'}];
+				self.games.forEach((game) =>{
+					strArray.push( game.genre );
+				})
+				//sets cannot contain duplicate values. voila! all genres no duplictes. 
+				strArray = Array.from( new Set(strArray));
+				//lets make an object list for easy handling.
+				strArray.forEach((genre) =>{
+					genreArray.push({ name : genre });
+				})
+				return genreArray;
 			}
 
 			self.addGame = function(){
 				return MGL_Task1_Service.createGame(self.game).then( function() {
-				self.fetchAllGames();
+					self.fetchAllGames();
+					self.game = {};
 				});
 			}
 			
@@ -39,6 +70,7 @@ angular.module('MGL_Task1_app').controller('MGL_Task1_Controller',
 			self.updateGame = function() {
 				return MGL_Task1_Service.updateGame(self.game).then( function() {
 					self.fetchAllGames();
+					self.game = {};
 				});
 			}
 
